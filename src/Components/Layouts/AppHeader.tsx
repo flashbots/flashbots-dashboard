7370ee7ad62ed6dec4fb4cba85c9af5f40c32e66
ryry79261 <ryanjnoble@gmail.com>
2021-05-05 14:40:21 +0200

    Copy added

--- src/Components/Layouts/AppHeader.tsx
+++ src/Components/Layouts/AppHeader.tsx
@@ -7,9 +7,9 @@ import {
 } from "@chainsafe/common-theme"
 import React, { useState } from "react"
 import clsx from "clsx"
-import { Button, HamburgerMenu, Link, NavLink, Typography } from "@chainsafe/common-components"
+import { Button, HamburgerMenu, Link, NavLink, Typography, useLocation } from "@chainsafe/common-components"
 import AppNav from "./AppNav"
-import { ROUTE_LINKS } from "../Routes"
+import { CURRENCY_TOGGLE_HIDDEN, ROUTE_LINKS } from "../Routes"
 import LogoPng from "../../Media/Robot_Emoji_OG.png"
 import { Currency, useDashboard } from "../../Contexts/DashboardContext/DashboardContext"
 import { Dashboard } from "../../Contexts/DashboardContext/Types"
@@ -143,6 +143,7 @@ const AppHeader = () => {
   const desktop = useMediaQuery(breakpoints.up("md"))
   const { endpoints } = useDashboard()
   const { currency, changeCurrency } = useDashboard()
+  const { pathname } = useLocation()
 
   return (
     <header
@@ -166,7 +167,12 @@ const AppHeader = () => {
             </NavLink>))
           }
           {
-            (
+            Object.keys(ROUTE_LINKS).filter((key: string) => typeof (ROUTE_LINKS as {[index: string]: any})[key] == "string").map((routeName: string, index: number) => (<NavLink className={classes.link} key={`nav-link-${index}`} to={`${(ROUTE_LINKS as {[index: string]: any})[routeName]}`} >
+              {routeName}
+            </NavLink>))
+          }
+          {
+            !CURRENCY_TOGGLE_HIDDEN.includes(pathname) && (
               <Button variant="outline" className={classes.currencySelector} onClick={() => changeCurrency(currency === Currency.USD ? Currency.ETH : Currency.USD  )}>
                 <svg version="1.1"  x="0px" y="0px" viewBox="0 0 1000 1000" enable-background="new 0 0 1000 1000" >
                   <g><path d="M417.2,91.8C281.5,200.6,145.8,309.5,10.1,418.4c326.6,0,653.1,0,979.7,0c-0.6-41.1,1.3-82.4-1-123.5c-6.6-38.6-48.1-42.1-79.5-39.8c-164,0-328.1,0-492.1,0C417.2,200.6,417.2,146.2,417.2,91.8z M10.1,581.6c0.6,41.1-1.3,82.4,1,123.5c6.6,38.6,48.1,42.1,79.5,39.8c164,0,328.1,0,492.1,0c0,54.4,0,108.9,0,163.3c135.7-108.9,271.4-217.7,407.1-326.6C663.3,581.6,336.7,581.6,10.1,581.6z"/></g>
--- src/Components/Routes.tsx
+++ src/Components/Routes.tsx
@@ -1,14 +1,25 @@
 import React from "react"
 import { Switch, Route, Redirect } from "@chainsafe/common-components"
 import DashboardPage from "./Pages/DashboardPage"
+import DataMetricsPage from "./Pages/DataMetricsPage"
 
 export const ROUTE_LINKS = {
+  "Data & Metrics": "/data-metrics",
   Dashboard: (dashboard: string) => `/${dashboard}`,
 }
+
+export const CURRENCY_TOGGLE_HIDDEN = [
+  ROUTE_LINKS["Data & Metrics"]
+]
 
 const FilesRoutes = () => {
   return (
     <Switch>
+      <Route
+        exact
+        path={ROUTE_LINKS["Data & Metrics"]}
+        component={DataMetricsPage}
+      />
       <Route
         exact
         path={ROUTE_LINKS.Dashboard(":dashboard")}
